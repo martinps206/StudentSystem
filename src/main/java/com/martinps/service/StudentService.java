c124063b96f50b76c37745dd9665e9f8036731a6
martinps206 <martinps_cc@hotmail.com>
2023-09-25 17:00:26 -0300

    Sistema Api con React Spring boot

---
+++ src/main/java/com/martinps/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.martinps.service;
+
+import com.martinps.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+}
---
+++ src/main/java/com/martinps/service/StudentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.martinps.service;
+
+import com.martinps.model.Student;
+
+import java.util.List;
+import com.martinps.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
